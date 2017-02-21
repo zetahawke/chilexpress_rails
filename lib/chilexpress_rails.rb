49a7e07ca50dc994b1331598e64c013c4cb4db59
michel <michel.szinavel@gmail.com>
2017-02-21 20:34:07 -0300

    adding gsub to delete special spaces into strings

--- lib/chilexpress_rails.rb
+++ lib/chilexpress_rails.rb
@@ -19,10 +19,10 @@ module ChilexpressRails
     li = widget_content[0].css('li')
     return 'El sitio web parece haber modificado su estructura' unless li
     {
-      track_number: li[1].text.to_s.split(':')[1].strip,
-      product: li[2].text.to_s.split(':')[1].strip,
-      service: li[3].text.to_s.split(':')[1].strip,
-      status: li[4].text.to_s.split(':')[1].strip
+      track_number: li[1].text.to_s.split(':')[1].strip.gsub(/\A\p{Space}*/, ''),
+      product: li[2].text.to_s.split(':')[1].strip.gsub(/\A\p{Space}*/, ''),
+      service: li[3].text.to_s.split(':')[1].strip.gsub(/\A\p{Space}*/, ''),
+      status: li[4].text.to_s.split(':')[1].strip.gsub(/\A\p{Space}*/, '')
     }
   end
 
@@ -30,12 +30,12 @@ module ChilexpressRails
     widget_content = html_doc.css('.wigdet-content')
     li = widget_content[1].css('li')
     return 'El sitio web parece haber modificado su estructura' unless li
-    date = li[2].text.to_s.split(':')[1].strip
-    hour = " #{li[3].text.to_s.split(':')[1].strip}:#{li[3].text.to_s.split(':')[2].strip}"
+    date = li[2].text.to_s.split(':')[1].strip.gsub(/\A\p{Space}*/, '')
+    hour = " #{li[3].text.to_s.split(':')[1].strip.gsub(/\A\p{Space}*/, '')}:#{li[3].text.to_s.split(':')[2].strip.gsub(/\A\p{Space}*/, '')}"
     {
-      receiver: li[1].text.to_s.split(':')[1].strip,
+      receiver: li[1].text.to_s.split(':')[1].strip.gsub(/\A\p{Space}*/, ''),
       datetime: date + hour,
-      receiver_name: li[4].text.to_s.split(':')[1].split(' ').first(2).join(' ')
+      receiver_name: li[4].text.to_s.split(':')[1].split(' ').first(2).join(' ').gsub(/\A\p{Space}*/, '')
     }
   end
 end
